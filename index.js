721dc21aaef51a45281db2a142aced6a1676709b
B-Madhubabu <madhubabubairu@gmail.com>
2025-08-12 17:48:37 +0530

    enhanced

--- index.js
+++ index.js
@@ -4,6 +4,35 @@ let GuestScoreEl = document.getElementById("Guest-Score")
 let HomeScore = 0
 let GuestScore = 0
 
+let HomeFaultEl = document.getElementById("Home-fault")
+let GuestFaultEl = document.getElementById("Guest-Fault")
+
+let HomeFaultCount = 0
+let GuestFaultCount = 0
+
+function Reset() {
+    HomeScore = 0
+    GuestScore = 0
+    HomeScoreEl.textContent = HomeScore
+    GuestScoreEl.textContent = GuestScore
+
+    HomeFaultCount = 0
+    GuestFaultCount = 0
+    HomeFaultEl.textContent = HomeFaultCount
+    GuestFaultEl.textContent = GuestFaultCount
+}
+
+function HomeFaultAdd1() {
+    HomeFaultCount++
+    HomeFaultEl.textContent = HomeFaultCount
+}
+
+function GuestFaultAdd1() {
+    GuestFaultCount++
+    GuestFaultEl.textContent = GuestFaultCount
+    // console.log(GuestFaultCount)
+}
+
 function HSadd1() {
     HomeScore += 1
     HomeScoreEl.textContent = HomeScore
